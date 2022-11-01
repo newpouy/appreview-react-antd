2f49863b7f29d19977f236b08040f7b2339cda49
WookyungYun <gyong1540@gmail.com>
2022-11-01 12:01:28 +0900

    initial commit

--- src/pages/AppReviewsPage.tsx
+++ src/pages/AppReviewsPage.tsx
@@ -3,7 +3,6 @@ import { useTranslation } from 'react-i18next';
 import { Tables } from '@app/components/tables/Tables/Tables';
 import { PageTitle } from '@app/components/common/PageTitle/PageTitle';
 // import httpApi from '@app/api/http.api';
-import axios from 'axios';
 import myHttp from '@app/api/myHttp';
 
 const AppReviewsPage: React.FC = () => {
